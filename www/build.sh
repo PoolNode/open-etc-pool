d90ec304f15082568e542f666f132c0bafbeb021
yuriy0803 <yuriy1989shevchuk@gmail.com>
2021-12-18 23:03:21 +0100

    Update

---
+++ www/build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# This is dirty af but will do for now.
+cp fix/intl-format-cache/src/* node_modules/intl-format-cache/src/
+cp fix/intl-format-cache/lib/* node_modules/intl-format-cache/lib/
+
+./node_modules/.bin/ember build --environment production
